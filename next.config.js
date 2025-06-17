aff5b28a6fa9edba1cfe4f975ef3bbe27dab139f
Royce O'Toole <royce@Royces-Mac-mini.local>
2025-06-17 17:15:32 -0500

    fix: add supabase storage to next.js image domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cmyyujlqxxiqgrnjaqqk.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig 
